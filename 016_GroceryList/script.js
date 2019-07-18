920fd072d8aef8b09c6c14183fe5014472879048
Daniele Vinci <vinci.dani@gmail.com>
2019-07-18 11:00:45 -0400

    js variables

---
+++ 016_GroceryList/script.js
@@ -0,0 +1,21 @@
+// variables
+const addItemsAction = document.querySelector('.addItems-action');
+const input = document.querySelector('.addItems-input');
+const submit = document.querySelector('.addItems-submit');
+const list = document.querySelector('.grocery-list');
+const displayItemsAction = document.querySelector('.displayItems-action');
+const clear = document.querySelector('.displayItems-clear');
+
+// event listeners
+submit.addEventListener('click', addItem);
+
+// functions
+function addItem(event) {
+  event.preventDefault();
+  let value = input.value;
+  if(value==="") {
+
+  } else {
+
+  }
+}
